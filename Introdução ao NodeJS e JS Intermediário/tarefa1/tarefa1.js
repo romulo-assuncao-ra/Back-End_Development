5269fe4b6b5a795fad776e3574c285e886dba770
Rômulo Assunção Corrêa <romulo.assuncao.ra@gmail.com>
2023-06-13 00:34:25 -0400

    Atividade realizada com assunto introdutório de nodeJS,  json e javascript

--- Introdução ao NodeJS e JS Intermediário/tarefa1/tarefa1.js
+++ Introdução ao NodeJS e JS Intermediário/tarefa1/tarefa1.js
@@ -6,13 +6,13 @@ class administrador{
         this.email = email,
         this.genero =genero
     }
-    infos = function(){
+    nomeEmail = function(){
         return this.name +" "+ this.email;
     }
 }
 
 class usuario extends administrador{
-    constructor( nome, idade, endereco, email, genero, usuario, senha){
+    constructor( nome, idade, endereco, email, genero, login, senha){
         super(
             nome,
             idade,
@@ -20,16 +20,28 @@ class usuario extends administrador{
             email,
             genero
         );
-        this.usuario = usuario,
+        this.login = login,
         this.senha = senha
         }
 
-        infos = function(){
-            return this.name +" "+ this.usuario +" "+ this.senha;
+        nomeLogin = function(){
+            return this.name +" "+ this.login +" "+ this.senha;
         }
     }
 
-    /*const user = new usuario("Rômulo",32,"rua cinco de maio","romulo@gmail.com","M", "assuncao",123456); 
-    console.log(user);*/
+    const user = new usuario("Rômulo",32,"rua cinco de maio","romulo@gmail.com","M", "assuncao",123456); 
+    
+    console.log(user);
 
-    const jsonLogin = '{"nome": "Rômulo", "idade":32, "endereco":"rua cinco de maio",   '
+    console.log(`nomeLogin`);
+
+
+    const jsonLogin = '{"nome": "Rômulo", "idade":32, "endereco":"rua cinco de maio", "email":"romulo@gamil.com","genero":"M","login":"assuncao","senha":123456}'
+
+    const obj =JSON.parse(jsonLogin);
+
+    obj.nome +=" Corrêa";
+    obj.departamento = "administrativo"
+
+    const jsonString = JSON.stringify(obj);
+    console.log(jsonString);
